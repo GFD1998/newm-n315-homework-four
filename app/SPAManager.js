fc3efd2fecfd0ef96ab86720e2f5ca79cbdc57f0
GFD1998 <gabrieldennett@gmail.com>
2023-03-12 22:23:55 -0400

    General Update

---
+++ app/SPAManager.js
@@ -0,0 +1,100 @@
+export default class SPAManager{
+
+
+    _model;
+
+
+    homeContent(){
+        $("#mainContainer").html(this._model.homeContent);
+        // window.location.hash = "home";
+    }
+
+    createContent(){
+        $("#mainContainer").html(this._model.createContent);
+        // window.location.hash = "home";
+    }
+
+    editContent(){
+        $("#mainContainer").html(this._model.editContent);
+        // window.location.hash = "home";
+    }
+
+    logInContent(){
+        $("#mainContainer").html(this._model.logInContent);
+        // window.location.hash = "home";
+    }
+
+    createAccountContent(){
+        $("#mainContainer").html(this._model.createAccountContent);
+        // window.location.hash = "home";
+    }
+
+    defaultContent(){
+        $("#mainContainer").html(this._model.defaultContent);
+        // window.location.hash = "home";
+    }
+
+
+
+    updateView(){
+        $('title').html(`${window.location.hash.split('#')[1]} Page`);
+        window.scrollTo(0, 0);
+        $("#mainMenu").css("display", "none");
+        this.setTriggers();
+    }
+
+
+    constructor(_model){
+        this._model = _model;
+        $(document).ready(e => {
+            this.updateView();
+            if($("#mainContainer").innerHTML == null){
+                $("#mainContainer").html(this.defaultContent());
+            }
+        });
+
+        $(window).on("hashchange", e => {
+            console.log("Hello from routeTo()" + window.location.hash);
+            switch(window.location.hash){
+                case "#HOME":
+                    this.homeContent();
+                    break;
+                case "#BROWSE":
+                    this.browseContent();
+                    break;
+                default:
+                    break;
+            }
+            this.updateView();
+        });
+
+        $("#hamburger").on("click", e =>{
+            if($("#mainMenu").css("display") == "none"){
+                $("#mainMenu").css("display", "block");
+            }else if($("#mainMenu").css("display") == "block"){
+                $("#mainMenu").css("display", "none");
+            }
+        });
+
+
+        $(window).on("resize", e => {
+            var pageWidth = e.currentTarget.innerWidth;
+            if(pageWidth >= 960){
+
+            }else{
+
+            }
+        });
+
+        this.setTriggers();
+    }
+
+    setTriggers(){
+        $(".imageContainerSection img").mouseenter(e => {
+            $(".overlayImageContainer").css("display", "block");
+        });
+        $(".overlayImageContainer").mouseleave(e => {
+            $(".overlayImageContainer").css("display", "none");
+        });
+    }
+}
